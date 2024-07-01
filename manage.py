fa13c4703f8833a5d414f7f75f706611783c7031
SwanLamirault <swan.lamirault@gmail.com>
2024-07-01 23:17:41 +0200

    tentative de correction de bug de port

--- manage.py
+++ manage.py
@@ -15,9 +15,11 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
 
-    port = os.environ.get('PORT', '8000')
-    sys.argv.append(f'0.0.0.0:{port}')
-    execute_from_command_line(sys.argv)
+    
+    # Ajoutez cette partie pour définir l'adresse et le port
+    if len(sys.argv) == 1:
+        port = os.environ.get('PORT', '8000')
+        sys.argv += ['runserver', f'0.0.0.0:{port}']
 
 
 if __name__ == '__main__':
